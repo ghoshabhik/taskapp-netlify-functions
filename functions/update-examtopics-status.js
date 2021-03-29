45e637f5cad3178a9a69c24df368d484313969e3
abhik <avik5324@gmail.com>
2021-03-29 12:53:54 +0530

    added update endpoints

---
+++ functions/update-examtopics-status.js
@@ -0,0 +1,37 @@
+
+const axios = require('axios')
+
+exports.handler = async (event, request, context) => {
+
+    const id = event.queryStringParameters['id']
+    const status = event.queryStringParameters['status']
+    const airtable_key = process.env.AIRTABLE_API_KEY
+
+    const config = {
+        headers: { Authorization: `Bearer ${airtable_key}` }
+    }
+    const url = 'https://api.airtable.com/v0/appnJyGDL7ilErfRQ/examtopics';
+    const body = {
+        records: [
+            {
+                id: id,
+                fields: {
+                    Status: status
+                }
+            }
+        ]
+    }
+    let res = await axios.patch(url, body, config)
+    
+    const respData = await res.data
+
+    return{
+        statusCode: 200,
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE'
+          },
+        body: JSON.stringify(respData)
+    }
+}
