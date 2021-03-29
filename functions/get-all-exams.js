7f4fc986f198cc417c0a476c2fb5224df538c511
abhik <avik5324@gmail.com>
2021-03-29 13:23:47 +0530

    exam

---
+++ functions/get-all-exams.js
@@ -0,0 +1,34 @@
+
+const axios = require('axios')
+
+exports.handler = async (event, request, context) => {
+
+
+    const airtable_key = process.env.AIRTABLE_API_KEY
+
+    const config = {
+        headers: { Authorization: `Bearer ${airtable_key}` }
+    }
+    const allExamsFromAirtable = await axios.get('https://api.airtable.com/v0/appnJyGDL7ilErfRQ/exams?view=Grid%20view', config)
+    const respData = await allExamsFromAirtable.data
+    let exams = []
+    console.log(respData)
+    respData.records.forEach( exam => {
+        exams.push({
+            name: exam.fields.ExamName,
+            remainingDays: exam.fields.RemainingDays,
+            noOfTopics: exam.fields.NoOfTopics,
+            noOfTopicsCompleted: exam.fields.NoOfTopicsCompleted,
+            noOfTopicsNotCompleted: exam.fields.NoOfTopics - exam.fields.NoOfTopicsCompleted
+        })
+    })
+    return{
+        statusCode: 200,
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE'
+          },
+        body: JSON.stringify(exams)
+    }
+}
